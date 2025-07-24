347e0c7abc70cc9c9bd2e1236a803a1008be5c30
AHINSA MOHANTY <ahinsamohanty12@gmail.com>
2025-07-24 14:34:46 +0530

    Added FindDuplicates.py

---
+++ Array/FindDuplicates.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def findDuplicates(self, nums):
+        res = []
+
+        for num in nums:
+            index = abs(num) - 1  
+
+            if nums[index] < 0:
+                res.append(abs(num))
+            else:
+                nums[index] *= -1  
+        return res
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
