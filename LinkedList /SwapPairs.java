5a8d52bf5753d1d8f9bd19d33aa2f478672c78ee
AHINSA MOHANTY <ahinsamohanty12@gmail.com>
2025-08-04 16:33:25 +0530

    Added SwapPairs.java

---
+++ LinkedList /SwapPairs.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        // Step 1: Detect if cycle exists
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                // Step 2: Find entry point of the cycle
+                slow = head;
+                while (slow != fast) {
+                    slow = slow.next;
+                    fast = fast.next;
+                }
+                return slow;
+            }
+        }
+
+        return null;
+        
+    }
+}
