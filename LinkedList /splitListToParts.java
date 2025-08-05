3960c5e67a12993bc44537e124249eac847a5ad7
AHINSA MOHANTY <ahinsamohanty12@gmail.com>
2025-08-05 18:09:54 +0530

    Added splitListToParts.java

---
+++ LinkedList /splitListToParts.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode[] splitListToParts(ListNode head, int k) {
+        ListNode[] result = new ListNode[k];
+        
+        int totalLength = 0;
+        ListNode curr = head;
+        while (curr != null) {
+            totalLength++;
+            curr = curr.next;
+        }
+
+        int partSize = totalLength / k;
+        int remainder = totalLength % k;
+
+        curr = head;
+        for (int i = 0; i < k; i++) {
+            result[i] = curr;
+            int currentPartSize = partSize + (i < remainder ? 1 : 0);
+
+            for (int j = 0; j < currentPartSize - 1; j++) {
+                if (curr != null) curr = curr.next;
+            }
+
+            if (curr != null) {
+                ListNode next = curr.next;
+                curr.next = null;
+                curr = next;
+            }
+        }
+
+        return result;
+        
+    }
+}
