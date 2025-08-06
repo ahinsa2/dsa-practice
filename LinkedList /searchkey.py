ec9d8f892edad3fe1e97fb60a1ec1bb5f10399a0
AHINSA MOHANTY <ahinsamohanty12@gmail.com>
2025-08-06 19:58:03 +0530

    Added searchkey.py

---
+++ LinkedList /searchkey.py
@@ -0,0 +1,18 @@
+#User function Template for python3
+
+
+'''Node of a linked list'''
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+        
+class Solution:
+    def searchKey(self, n, head, key):
+        #Code here
+        current = head
+        while current:
+            if current.data == key:
+                return True
+            current = current.next
+        return False
