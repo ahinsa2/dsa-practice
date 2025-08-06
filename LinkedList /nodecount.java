d1ff245403c19d94e68d925579dc8ec62470278d
AHINSA MOHANTY <ahinsamohanty12@gmail.com>
2025-08-06 10:43:06 +0530

    Added nodecount.java

---
+++ LinkedList /nodecount.java
@@ -0,0 +1,19 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class Solution {
+    public int countNodes(ListNode head) {
+        int count = 0;
+        while (head != null) {
+            count++;
+            head = head.next;
+        }
+        return count;
+    }
+}
