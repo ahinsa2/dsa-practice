bfbd599c619f6000c1f9fbf8813a10bf3bf339f1
AHINSA MOHANTY <ahinsamohanty12@gmail.com>
2025-08-20 15:00:07 +0530

    Create ArrayList.java

---
+++ Array/ArrayList.java
@@ -0,0 +1,29 @@
+class Solution {
+    static ArrayList<Integer> leaders(int arr[]) {
+        // code here
+        iint n = arr.length;
+        ArrayList<Integer> result = new ArrayList<>();
+
+        
+        int maxFromRight = arr[n - 1];
+        result.add(maxFromRight);
+
+        
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] >= maxFromRight) {
+                maxFromRight = arr[i];
+                result.add(maxFromRight);
+            }
+        }
+
+        
+        ArrayList<Integer> leadersList = new ArrayList<>();
+        for (int i = result.size() - 1; i >= 0; i--) {
+            leadersList.add(result.get(i));
+        }
+
+        return leadersList;
+    }
+        
+    }
+}
