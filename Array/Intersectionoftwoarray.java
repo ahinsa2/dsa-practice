6faeca0b2853c5d81194f58454786b758e158985
AHINSA MOHANTY <ahinsamohanty12@gmail.com>
2025-09-21 18:56:26 +0530

    Update Intersectionoftwoarray.java

--- Array/Intersectionoftwoarray.java
+++ Array/Intersectionoftwoarray.java
@@ -3,7 +3,7 @@ class Solution {
         Set<Integer> set1 = new HashSet<>();
         for (int num : nums1) {
             set1.add(num);
-        }
+        }//
 
         Set<Integer> resultSet = new HashSet<>();
         for (int num : nums2) {
