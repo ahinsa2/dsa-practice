125fb40167524eee7d498b2908d90869bc3e272f
AHINSA MOHANTY <ahinsamohanty12@gmail.com>
2025-09-28 16:56:26 +0530

    Update delmid.py

--- delmid.py
+++ delmid.py
@@ -19,7 +19,7 @@ class Solution(object):
 
         prev.next = slow.next
         return head
-        """
+        """123456
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
