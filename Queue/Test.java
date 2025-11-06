a399f5d010f606cebeb6cf5e15d06757973ab894
AHINSA MOHANTY <ahinsamohanty12@gmail.com>
2025-11-07 00:58:25 +0530

    Create Test.java

---
+++ Queue/Test.java
@@ -0,0 +1,19 @@
+class Test{
+    static int findKthSmallest(int[] arr, int k) {
+        int n = arr.length;
+
+        // Selection Sort
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex])
+                    minIndex = j;
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
+        return arr[k - 1];
+    }}
+
+    
