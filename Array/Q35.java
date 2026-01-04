287bad91bb89acce76b395c75c0b720a7015bbc5
AHINSA MOHANTY <ahinsamohanty12@gmail.com>
2026-01-04 20:49:43 +0530

    Create Q35.java

---
+++ Array/Q35.java
@@ -0,0 +1,19 @@
+class Q35 {
+    public int searchInsert(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return low;
+        
+    }
+}
